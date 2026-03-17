ae8d6b4b21d530e0f2a2e55a9bda44684e6fecbf
Nachipatel03 <nachipatel003@outlook.com>
2026-03-17 12:36:18 +0530

    add config

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  server: {
+    host: true
+  },
+
+  preview: {
+    host: true,
+    allowedHosts: 'all'
+  }
 })
